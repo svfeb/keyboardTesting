e4651daee2928ba89839693978a3eb41964361f1
Shubham Verma <106722629+svfeb@users.noreply.github.com>
2022-09-04 14:05:28 +0530

    Updated

--- src/components/keyboard.js
+++ src/components/keyboard.js
@@ -99,24 +99,62 @@ function Keyboard() {
         >
           Press the keys on your keyboard to run the test
         </p>
-
-        <div className="row">
-          <div className="group">
-            {Object.keys(keyboardRows).map((row, i) => {
-              let _row = row + i;
-              return (
-                <div className="row" key={_row}>
-                  {keyboardRows[row].map((keys, i) => {
-                    let _id = keys + i;
-                    return (
-                      <div className="key" data-key={keys} key={_id}>
-                        {keys}
-                      </div>
-                    );
-                  })}
-                </div>
-              );
-            })}
+        <div>
+          <div>
+            <div className="group">
+              {Object.keys(keyboardRows).map((row, i) => {
+                let _row = row + i;
+                return (
+                  <div className="row" key={_row}>
+                    {keyboardRows[row].map((keys, i) => {
+                      let _id = keys + i;
+                      if (keys === "Windows")
+                        return (
+                          <div data-key={keys} key={_id} className="key">
+                            <i className="fab fa-windows"></i>
+                          </div>
+                        );
+                      else if (keys === "Backspace")
+                        return (
+                          <div
+                            data-key={keys}
+                            key={_id}
+                            className="key backspace"
+                          >
+                            <i className="fas fa-arrow-left-long"></i>
+                          </div>
+                        );
+                      else if (keys === "Shift")
+                        return (
+                          <div
+                            data-key={keys}
+                            key={_id}
+                            className="key shift r-s"
+                          >
+                            <span>Shift</span>
+                          </div>
+                        );
+                      else if (keys === "Space")
+                        return (
+                          <div
+                            data-key={keys}
+                            key={_id}
+                            className="key spaceBar"
+                          >
+                            Space
+                          </div>
+                        );
+                      else
+                        return (
+                          <div className="key" data-key={keys} key={_id}>
+                            {keys}
+                          </div>
+                        );
+                    })}
+                  </div>
+                );
+              })}
+            </div>
           </div>
         </div>
       </div>
